ac75cb8ec80cbfc9de5c4b194626bd4a608354e5
Iranduba <mjrc4q5@gmail.com>
2021-07-02 14:48:02 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+echo "# PrimeiroApp" >> README.md
---
+++ app/src/main/java/com/example/primeiroapp/TesteArray.kt
@@ -0,0 +1,10 @@
+package com.example.primeiroapp
+
+fun main () {
+    val valores = IntArray(size = 3)
+    valores[0] = 1
+    valores[1] = 4
+    for(valor in valores)
+    println(valor)
+
+}
